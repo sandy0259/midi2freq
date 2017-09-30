6ea28ba1f5b72088b848b4ead22a122be7e8f140
Shane T Wirkes <swirke2@shane-wirkess-macbook.local>
2017-09-30 18:29:10 +0300

    Added notes and removed a superfluous error message.

--- midi2freq/main.c
+++ midi2freq/main.c
@@ -1,7 +1,7 @@
 /*
-This code is taken from The Audio Programming Book, Chapter 1, Listing 1.3
-It has been updated to use 'fgets(char *restrict, int, FILE *)' instead of
-'gets()' as that function has been depricated and is dangerous to use.
+ This code is taken from The Audio Programming Book, Chapter 1, Listing 1.3
+ It has been updated to use 'fgets(char *restrict, int, FILE *)' instead of
+ 'gets()' as that function has been depricated and is dangerous to use.
 */
 #include <stdlib.h>
 #include <stdio.h>
