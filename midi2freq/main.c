013282b3f3ecbe0cad188a9f9a529ceb7329497e
Shane T Wirkes <swirke2@shane-wirkess-macbook.local>
2017-10-01 13:34:41 +0300

    updated to use 'scanf("%d", &midinote)' instead of 'fgets'

--- midi2freq/main.c
+++ midi2freq/main.c
@@ -1,6 +1,6 @@
 /*
  This code is taken from The Audio Programming Book, Chapter 1, Listing 1.3
- It has been updated to use 'fgets(char *restrict, int, FILE *)' instead of
+ It has been updated to use 'scanf("%d", &midinote)' instead of
  'gets()' as that function has been depricated and is dangerous to use.
 */
 #include <stdlib.h>
@@ -12,12 +12,11 @@ int main() {
     double semitone_ratio;
     double c0; //for frequency of MIDI Note 0
     double c5; //for frequency of Middle C
-    double pow(double, double);
     double frequency; //...which we want to find
-    int midinote; //...given this note
-    char message[256];
-    char* result;
-    
+    int midinote = 150; //...given this note
+    //setting midinote to arbitrary invalid value so that if user does not
+    //enter an int, it will return with an error.
+
     //calculate the required numbers
     semitone_ratio = pow(2, 1/12.0);
     //find Middle C, three semitones above low A = 220;
@@ -30,27 +29,21 @@ int main() {
     //exactly how big the entry will be. Modern compilers will
     //warn that this is a dangerous function, but they will still
     //support it.
-    //instead use: fgets(<#char *restrict#>, <#int#>, <#FILE *#>)
-    //if getting input from stdio, use 'stdin' for the third argument
-    result = fgets(message, 5, stdin);
-    if(result == NULL){
-        printf("There was an error reading the input.\n");
+    //instead use: scanf to take an int.
+    
+    scanf("%d", &midinote);
+    //check for erroneous input
+    if(midinote == 150){
+        printf("Please enter a valid integer 0-127\n");
         return 1;
     }
-    if(message[0] == '\0'){
-        printf("Have a nice day.\n");
-        return 1;
-    }
-    
-    midinote = atoi(message);
-    
     if(midinote < 0){
-        printf("Sorry - %s is a bad MIDI note number\n", message);
-        return 1;
+        printf("Please enter a valid integer 0-127\n");
+        return 2;
     }
     if(midinote > 127){
-        printf("Sorry - that number is beyond the MIDI range.\n");
-        return 1;
+        printf("Please enter a valid integer 0-127.\n");
+        return 3;
     }
     //calculate a frequency for a given Midi Note Number
     frequency = c0 * pow(semitone_ratio, midinote);
